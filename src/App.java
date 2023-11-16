b3f288cfc2e055a0c3a416e54227ca1574259a11
Maria Eliana - Dev\Projeto-base <elianamotadeoliveira@gmail.com>
2023-11-15 21:05:00 -0300

    first commit

---
+++ src/App.java
@@ -0,0 +1,47 @@
+public class App {
+    public static void main(String[] args) {
+
+       /* String nome;
+        nome="Iago";
+        nome="Théo";
+        nome="Eliana";
+
+        System.out.println("Hello, " + nome);*/
+
+         /*int a = 3;
+        int b = 2;
+        int soma = a + b;
+        int subtração = a - b;
+        int multiplicacao = a * b;
+        float divisao = (float) a / b; 
+
+        System.out.println(soma);
+        System.out.println(subtração);
+        System.out.println(multiplicacao);
+        System.out.println(divisao);*/
+
+        /*Tabela verdade
+
+        Operador "&&"" (and)
+        true && true = true
+        true && false = false
+        false && true = false
+        false && false = false
+
+        Operador "||" (or)
+
+        true || true = true
+        true || false = true
+        false || true = true
+        false || false = false*/
+
+        /*Boolean fimDeSemana = false;
+         Boolean fazendoSol = false;
+         Boolean vamosAPraia = fimDeSemana && fazendoSol;
+         System.out.println(vamosAPraia);
+         String mensagem = fimDeSemana ? "É fim de semana" : "Não é fim de semana";
+         System.out.println(mensagem);*/
+
+         System.out.println();
+    }
+}
